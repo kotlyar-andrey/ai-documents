5a7b24258019fb0f055dbbd9ba031bb132b89361
Andrey Kotlyar <kotlyar562@gmail.com>
2025-07-21 11:37:08 +0300

    Init react-vite-typescipt-tailwindcss frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='bg-gray-800 min-h-screen text-gray-200'>
+      <div className='mx-auto max-w-screen-lg'>
+        <h1 className='text-center text-2xl'>Ai for working with documents</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
